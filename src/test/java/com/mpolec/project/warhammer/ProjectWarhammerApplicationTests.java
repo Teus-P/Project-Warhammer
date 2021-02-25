8f0a6086ede749be40d8bdf493eb93e911ccf340
Mateonidas <teus.polec@gmail.com>
2021-02-25 18:14:29 +0100

    Initial commit

---
+++ src/test/java/com/mpolec/project/warhammer/ProjectWarhammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpolec.project.warhammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWarhammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
